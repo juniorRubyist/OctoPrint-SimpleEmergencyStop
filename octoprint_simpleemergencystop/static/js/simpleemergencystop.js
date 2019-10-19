e203bb8a3e3046d98506cad1562dd68a7019858e
Joseph Geis <juniorrubyist@gmail.com>
2019-10-19 14:01:57 -0700

    Added button label option

--- octoprint_simpleemergencystop/static/js/simpleemergencystop.js
+++ octoprint_simpleemergencystop/static/js/simpleemergencystop.js
@@ -15,46 +15,46 @@ $(function() {
 
         self.onAfterBinding = function() {
             self.confirmation = $("#confirmation");
-            self.settings = self.allSettings.settings.plugins.simpleemergencystop;
+            self.settings =
+                self.allSettings.settings.plugins.simpleemergencystop;
         };
 
-        self.click = function () {
-            if(self.settings.confirmationDialog())
+        self.click = function() {
+            if (self.settings.confirmationDialog())
                 self.confirmation.modal("show");
-            else
-                self.sendCommand()
-
+            else self.sendCommand();
         };
 
-        self.sendCommand = function () {
+        self.sendCommand = function() {
             $.ajax({
-                 url: API_BASEURL+"plugin/simpleemergencystop",
-                 type: "POST",
-                 dataType: "json",
-                 data: JSON.stringify({
-                     command: "emergencyStop"
-                 }),
-                 contentType: "application/json; charset=UTF-8",
-                 success: function (data,status) {
-
-                 }
+                url: API_BASEURL + "plugin/simpleemergencystop",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    command: "emergencyStop"
+                }),
+                contentType: "application/json; charset=UTF-8",
+                success: function(data, status) {}
             });
             self.confirmation.modal("hide");
-
         };
 
-        self.visibleTest = function () {
-            return  self.loginState.isUser() && self.printerState.isOperational()
+        self.visibleTest = function() {
+            return (
+                self.loginState.isUser() && self.printerState.isOperational()
+            );
         };
 
-
+        self.showText = function() {
+            return self.settings.showText();
+        };
     }
 
     // view model class, parameters for constructor, container to bind to
     OCTOPRINT_VIEWMODELS.push([
         SimpleemergencystopViewModel,
 
-        ["settingsViewModel","loginStateViewModel","printerStateViewModel"],
+        ["settingsViewModel", "loginStateViewModel", "printerStateViewModel"],
 
         ["#navbar_plugin_simpleemergencystop"]
     ]);
